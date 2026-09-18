08360b6edf1a2705629a9f29f5a51af776b57eb8
TheMaJunjian <mx37163@163.com>
2026-09-18 17:25:30 +0800

    特殊值处理

--- app.js
+++ app.js
@@ -689,7 +689,7 @@
   var nativeScrollCorrectionControl = null;
   var nativeScrollCorrectionEpoch = 0;
   var NATIVE_SCROLL_CORRECTION_DELAY_MS = 500;
-  var NATIVE_SCROLL_CORRECTION_DURATION_MS = 10;
+  var NATIVE_SCROLL_CORRECTION_DURATION_MS = 700;
 
   function cancelNativeScrollCorrection() {
     nativeScrollCorrectionEpoch += 1;
